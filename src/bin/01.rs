e35b43d0d2550f0b2a4b07fab1f31bf01b3caf0f
Alex McHugh <alexmchughnz@icloud.com>
2023-08-01 21:19:23 +1200

    Solved 01-2.

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -12,7 +12,20 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let elves = input.split("\n\n");
+
+    let calories = elves.map(|elf| {
+        elf.trim()
+            .split('\n')
+            .map(|item| item.parse::<u32>().unwrap())
+            .sum::<u32>()
+    });
+
+    let mut sorted_calories: Vec<u32> = calories.collect();
+    sorted_calories.sort();
+    sorted_calories.reverse();
+
+    Some(sorted_calories[0..3].into_iter().sum())
 }
 
 fn main() {
@@ -34,6 +47,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 1);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(45000));
     }
 }
