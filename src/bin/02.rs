1becb255d6c95422cd8ff89805e2818c89b432a8
Alex McHugh <alexmchughnz@icloud.com>
2023-08-11 21:09:26 +1200

    Solved 02-2.

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -7,6 +7,7 @@ enum Shape {
 use Shape::*;
 const SHAPE_ORDER: [Shape; 3] = [Rock, Paper, Scissors]; // Higher index beats lower index, e.g. Paper > Rock
 
+#[derive(Clone, PartialEq)]
 enum Outcome {
     Win,
     Lose,
@@ -14,7 +15,7 @@ enum Outcome {
 }
 use Outcome::*;
 
-fn map_elf_char(char: &str) -> Shape {
+fn elf_char_to_shape(char: &str) -> Shape {
     match char {
         "A" => Rock,
         "B" => Paper,
@@ -23,11 +24,20 @@ fn map_elf_char(char: &str) -> Shape {
     }
 }
 
-fn map_my_char(char: &str) -> Shape {
+fn my_char_to_shape(char: &str) -> Shape {
     match char {
         "X" => Rock,
         "Y" => Paper,
         "Z" => Scissors,
+        _ => panic!("Invalid shape"),
+    }
+}
+
+fn my_char_to_outcome(char: &str) -> Outcome {
+    match char {
+        "X" => Lose,
+        "Y" => Draw,
+        "Z" => Win,
         _ => panic!("Invalid shape"),
     }
 }
@@ -48,7 +58,7 @@ fn score_outcome(outcome: &Outcome) -> u32 {
     }
 }
 
-fn get_outcome_for_game(elf_shape: &Shape, my_shape: &Shape) -> Outcome {
+fn determine_outcome_for_game(elf_shape: &Shape, my_shape: &Shape) -> Outcome {
     if elf_shape == my_shape {
         return Draw;
     }
@@ -67,20 +77,51 @@ fn get_outcome_for_game(elf_shape: &Shape, my_shape: &Shape) -> Outcome {
     }
 }
 
+fn determine_shape_for_outcome(elf_shape: &Shape, outcome: &Outcome) -> Shape {
+    match outcome {
+        Draw => elf_shape.clone(),
+        Win => SHAPE_ORDER
+            .into_iter()
+            .cycle()
+            .skip_while(|s| s != elf_shape)
+            .skip(1) // Elf's shape
+            .next() // Winner vs elf's shape
+            .expect("cycle iteration should never end"),
+        Lose => SHAPE_ORDER
+            .into_iter()
+            .rev()
+            .cycle()
+            .skip_while(|s| s != elf_shape)
+            .skip(1) // Elf's shape
+            .next() // Loser vs elf's shape
+            .expect("cycle iteration should never end"),
+    }
+}
+
 pub fn part_one(input: &str) -> Option<u32> {
     let mut score: u32 = 0;
     for mut line in input.lines().map(|l| l.split_whitespace()) {
-        let elf_shape = map_elf_char(line.next().unwrap());
-        let my_shape = map_my_char(line.next().unwrap());
+        let elf_shape = elf_char_to_shape(line.next().unwrap());
+        let my_shape = my_char_to_shape(line.next().unwrap());
 
         score += score_shape(&my_shape);
-        score += score_outcome(&get_outcome_for_game(&elf_shape, &my_shape));
+        score += score_outcome(&determine_outcome_for_game(&elf_shape, &my_shape));
     }
     Some(score)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let mut score: u32 = 0;
+    for mut line in input.lines().map(|l| l.split_whitespace()) {
+        let elf_shape = elf_char_to_shape(line.next().unwrap());
+        let outcome = my_char_to_outcome(line.next().unwrap());
+
+        let my_shape = determine_shape_for_outcome(&elf_shape, &outcome);
+
+        score += score_shape(&my_shape);
+        score += score_outcome(&outcome);
+    }
+    Some(score)
 }
 
 fn main() {
@@ -102,6 +143,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 2);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(12));
     }
 }
