a6040353e8af4dcf2478c7ec07a63f1cce974d42
Alex McHugh <alexmchughnz@icloud.com>
2023-08-11 22:12:53 +1200

    Solved 03-1.

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -1,5 +1,28 @@
+use std::collections::HashSet;
+
+fn item_to_priority(item: &char) -> u32 {
+    let mut priority = 1 + item.to_ascii_lowercase() as u32 - 'a' as u32;
+    if item.is_ascii_uppercase() {
+        priority += 26;
+    }
+    priority
+}
+
 pub fn part_one(input: &str) -> Option<u32> {
-    None
+    let mut sum_priorities: u32 = 0;
+    for line in input.lines() {
+        let rucksack_a = &line[..line.len() / 2];
+        let rucksack_b = &line[line.len() / 2..];
+        let items_a: HashSet<char> = HashSet::from_iter(rucksack_a.chars());
+        let items_b: HashSet<char> = HashSet::from_iter(rucksack_b.chars());
+
+        let duplicate = items_a
+            .intersection(&items_b)
+            .next()
+            .expect("Rucksacks should share one item");
+        sum_priorities += item_to_priority(duplicate);
+    }
+    Some(sum_priorities)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
