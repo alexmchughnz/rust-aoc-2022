a5129aa3d82c1bdc9113afba3d02ed0a9c87e870
Alex McHugh <alexmchughnz@icloud.com>
2023-08-12 12:06:01 +1200

    Solved 04-1.

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -1,5 +1,21 @@
+use core::num;
+
+fn is_subrange(range: (u32, u32), cand: (u32, u32)) -> bool {
+    range.0 <= cand.0 && cand.1 <= range.1
+}
+
 pub fn part_one(input: &str) -> Option<u32> {
-    None
+    let mut num_overlapping: u32 = 0;
+    for line in input.lines() {
+        let mut nums = line.split(['-', ',']).map(|s| s.parse::<u32>().unwrap());
+        let range_1 = (nums.next().unwrap(), nums.next().unwrap());
+        let range_2 = (nums.next().unwrap(), nums.next().unwrap());
+
+        if is_subrange(range_1, range_2) || is_subrange(range_2, range_1) {
+            num_overlapping += 1;
+        }
+    }
+    Some(num_overlapping)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
