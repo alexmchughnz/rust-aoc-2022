8de6c1bda261ec24e9e95da0d4221b39b1bf7b8a
Alex McHugh <alexmchughnz@icloud.com>
2023-08-12 23:06:04 +1200

    05-1: Solved.

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -1,10 +1,11 @@
 type CrateStructure = Vec<Vec<char>>;
+type Move = (u32, usize, usize);
 
-fn read_crates(state: &str) -> CrateStructure {
+fn read_crates(drawing: &str) -> CrateStructure {
     const CRATE_OFFSET: usize = 1;
     const CRATE_WIDTH: usize = 4;
 
-    let mut lines: Vec<&str> = state.split("\n").collect();
+    let mut lines: Vec<&str> = drawing.split("\n").collect();
 
     let num_stacks = lines.pop().unwrap().split_whitespace().count();
     let mut structure: CrateStructure = vec![Vec::<char>::new(); num_stacks];
@@ -20,11 +21,45 @@ fn read_crates(state: &str) -> CrateStructure {
     structure
 }
 
+fn read_moves(procedure: &str) -> Vec<Move> {
+    let mut moves = Vec::<Move>::new();
+    for line in procedure.lines() {
+        let mut nums_iter = line
+            .split_whitespace()
+            .filter_map(|w| w.parse::<usize>().ok());
+        let nums = (
+            nums_iter.next().unwrap() as u32, // Number of crates to move
+            nums_iter.next().unwrap() - 1,    // Index of src stack
+            nums_iter.next().unwrap() - 1,    // Index of dest stack
+        );
+        moves.push(nums);
+    }
+    moves
+}
+
+fn perform_moves(crates: &mut CrateStructure, moves: Vec<Move>) {
+    for (num, src, dest) in moves {
+        for _ in 0..num {
+            let c = crates[src].pop().expect("Should not move from empty stack");
+            crates[dest].push(c);
+        }
+    }
+}
+
 pub fn part_one(input: &str) -> Option<String> {
     let mut info = input.split("\n\n");
-    let (state, moves) = (info.next().unwrap(), info.next().unwrap());
-    let crates = read_crates(state);
-    None
+    let (drawing, procedure) = (info.next().unwrap(), info.next().unwrap());
+
+    let mut crates = read_crates(drawing);
+    let moves = read_moves(procedure);
+    perform_moves(&mut crates, moves);
+
+    let top_crates = crates
+        .into_iter()
+        .map(|stack| stack.last().unwrap().clone())
+        .collect();
+
+    Some(top_crates)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
