95cb6b3a9256104baa8aa460d847aa6632078d75
Alex McHugh <alexmchughnz@icloud.com>
2023-08-12 13:22:33 +1200

    Solved 04-2.

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -1,25 +1,45 @@
-use core::num;
+fn fully_contains(a: (u32, u32), b: (u32, u32)) -> bool {
+    let range_a = a.0..=a.1;
+    let start_in_a = range_a.contains(&b.0);
+    let end_in_a = range_a.contains(&b.1);
 
-fn is_subrange(range: (u32, u32), cand: (u32, u32)) -> bool {
-    range.0 <= cand.0 && cand.1 <= range.1
+    start_in_a && end_in_a
+}
+
+fn contains(a: (u32, u32), b: (u32, u32)) -> bool {
+    let range_a = a.0..=a.1;
+    let start_in_a = range_a.contains(&b.0);
+    let end_in_a = range_a.contains(&b.1);
+
+    start_in_a || end_in_a
 }
 
 pub fn part_one(input: &str) -> Option<u32> {
+    let mut num_fully_contained: u32 = 0;
+    for line in input.lines() {
+        let mut nums = line.split(['-', ',']).map(|s| s.parse::<u32>().unwrap());
+        let range_1 = (nums.next().unwrap(), nums.next().unwrap());
+        let range_2 = (nums.next().unwrap(), nums.next().unwrap());
+
+        if fully_contains(range_1, range_2) || fully_contains(range_2, range_1) {
+            num_fully_contained += 1;
+        }
+    }
+    Some(num_fully_contained)
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
     let mut num_overlapping: u32 = 0;
     for line in input.lines() {
         let mut nums = line.split(['-', ',']).map(|s| s.parse::<u32>().unwrap());
         let range_1 = (nums.next().unwrap(), nums.next().unwrap());
         let range_2 = (nums.next().unwrap(), nums.next().unwrap());
 
-        if is_subrange(range_1, range_2) || is_subrange(range_2, range_1) {
+        if contains(range_1, range_2) || contains(range_2, range_1) {
             num_overlapping += 1;
         }
     }
     Some(num_overlapping)
-}
-
-pub fn part_two(input: &str) -> Option<u32> {
-    None
 }
 
 fn main() {
@@ -41,6 +61,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 4);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(4));
     }
 }
