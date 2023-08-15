6c1b24eb6fb8847615ad0a8aef63e0bfd88e6129
Alex McHugh <alexmchughnz@icloud.com>
2023-08-15 15:51:56 +1200

    08-1: Solved.

--- src/bin/08.rs
+++ src/bin/08.rs
@@ -1,5 +1,55 @@
+type Forest = Box<[Box<[u8]>]>;
+
+fn parse_forest(input: &str) -> Forest {
+    let mut forest = Vec::<Box<[u8]>>::new();
+
+    for line in input.lines() {
+        let row = line
+            .chars()
+            .map(|c| c.to_digit(10).unwrap() as u8)
+            .collect::<Vec<u8>>()
+            .into_boxed_slice();
+        forest.push(row);
+    }
+
+    forest.into_boxed_slice()
+}
+
+fn is_highest(tree: &u8, line: &Vec<&u8>) -> bool {
+    line.into_iter().all(|t| tree > *t)
+}
+
 pub fn part_one(input: &str) -> Option<u32> {
-    None
+    let forest = parse_forest(input);
+
+    let n_cols = forest.len();
+    let n_rows = forest[0].len();
+
+    let mut n_visible = n_cols * 2 + (n_rows - 2) * 2; // Outside edges are already visible
+
+    // Iterate over inner trees.
+    for r in 1..n_rows - 1 {
+        let row = &forest[r];
+        for c in 1..n_cols - 1 {
+            let height = &row[c];
+            let above = forest[0..r].iter().map(|row| &row[c]).collect::<Vec<_>>();
+            let below = forest[r + 1..n_rows]
+                .iter()
+                .map(|row| &row[c])
+                .collect::<Vec<_>>();
+            let left = forest[r][0..c].iter().collect::<Vec<_>>();
+            let right = forest[r][c + 1..n_cols].iter().collect::<Vec<_>>();
+
+            if [above, below, left, right]
+                .iter()
+                .any(|seq| is_highest(height, seq))
+            {
+                n_visible += 1;
+            }
+        }
+    }
+
+    Some(n_visible as u32)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
